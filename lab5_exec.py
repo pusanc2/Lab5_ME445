26300b707c1bc78c7149f6c9f1cf2264c3a9f612
Pusan Chakraborty <pusanc2@illinois.edu>
2025-04-22 11:12:38 -0500

    final exec update

--- lab5_exec.py
+++ lab5_exec.py
@@ -10,40 +10,22 @@ from lab5_header import *
 from lab5_func import *
 from blob_search import *
 
-
 # ========================= Student's code starts here =========================
 
-# Position for UR3 not blocking the camera
 go_away = [270*PI/180.0, -90*PI/180.0, 90*PI/180.0, -90*PI/180.0, -90*PI/180.0, 135*PI/180.0]
 
-# Store world coordinates of green and yellow blocks
 xw_yw_G = []
 xw_yw_Y = []
 
-# xw_yw_G_end1 = [0.3, 0.3, 0.02]
-# xw_yw_Y_end1 = [0.3, 0.35, 0.02]
-
-# xw_yw_G_end2 = [0.35, 0.3, 0.02]
-# xw_yw_Y_end2 = [0.35, 0.35, 0.02]
-
 Dest = [[0.20, 0.25], [0.20, 0.30],[0.15, 0.25],[0.15, 0.30]]
-# Any other global variable you want to define
-# Hints: where to put the blocks?
-
 
 # ========================= Student's code ends here ===========================
 
-################ Pre-defined parameters and functions no need to change below ################
-
-# 20Hz
 SPIN_RATE = 20
 
-# UR3 home location
 home = [0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0]
 
-# UR3 current position, using home position for initialization
 current_position = copy.deepcopy(home)
-
 thetas = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 digital_in_0 = 0
@@ -57,48 +39,23 @@ current_position_set = False
 
 image_shape_define = False
 
-
-"""
-Whenever ur3/gripper_input publishes info this callback function is called.
-"""
 def input_callback(msg):
-
     global digital_in_0
     digital_in_0 = msg.DIGIN
-    digital_in_0 = digital_in_0 & 1 # Only look at least significant bit, meaning index 0
+    digital_in_0 = digital_in_0 & 1
 
-
-"""
-Whenever ur3/position publishes info, this callback function is called.
-"""
 def position_callback(msg):
-
     global thetas
     global current_position
     global current_position_set
 
-    thetas[0] = msg.position[0]
-    thetas[1] = msg.position[1]
-    thetas[2] = msg.position[2]
-    thetas[3] = msg.position[3]
-    thetas[4] = msg.position[4]
-    thetas[5] = msg.position[5]
-
-    current_position[0] = thetas[0]
-    current_position[1] = thetas[1]
-    current_position[2] = thetas[2]
-    current_position[3] = thetas[3]
-    current_position[4] = thetas[4]
-    current_position[5] = thetas[5]
+    for i in range(6):
+        thetas[i] = msg.position[i]
+        current_position[i] = thetas[i]
 
     current_position_set = True
 
-
-"""
-Function to control the suction cup on/off
-"""
 def gripper(pub_cmd, loop_rate, io_0):
-
     global SPIN_RATE
     global thetas
     global current_io_0
@@ -118,35 +75,21 @@ def gripper(pub_cmd, loop_rate, io_0):
     pub_cmd.publish(driver_msg)
 
     while(at_goal == 0):
-
-        if( abs(thetas[0]-driver_msg.destination[0]) < 0.0005 and \
-            abs(thetas[1]-driver_msg.destination[1]) < 0.0005 and \
-            abs(thetas[2]-driver_msg.destination[2]) < 0.0005 and \
-            abs(thetas[3]-driver_msg.destination[3]) < 0.0005 and \
-            abs(thetas[4]-driver_msg.destination[4]) < 0.0005 and \
-            abs(thetas[5]-driver_msg.destination[5]) < 0.0005 ):
-
-            #rospy.loginfo("Goal is reached!")
+        if all(abs(thetas[i] - driver_msg.destination[i]) < 0.0005 for i in range(6)):
             at_goal = 1
 
         loop_rate.sleep()
 
-        if(spin_count >  SPIN_RATE*5):
-
+        if(spin_count > SPIN_RATE*5):
             pub_cmd.publish(driver_msg)
             rospy.loginfo("Just published again driver_msg")
             spin_count = 0
 
-        spin_count = spin_count + 1
+        spin_count += 1
 
     return error
 
-
-"""
-Move robot arm from one position to another
-"""
 def move_arm(pub_cmd, loop_rate, dest, vel, accel):
-
     global thetas
     global SPIN_RATE
 
@@ -164,95 +107,68 @@ def move_arm(pub_cmd, loop_rate, dest, vel, accel):
     loop_rate.sleep()
 
     while(at_goal == 0):
-
-        if( abs(thetas[0]-driver_msg.destination[0]) < 0.0005 and \
-            abs(thetas[1]-driver_msg.destination[1]) < 0.0005 and \
-            abs(thetas[2]-driver_msg.destination[2]) < 0.0005 and \
-            abs(thetas[3]-driver_msg.destination[3]) < 0.0005 and \
-            abs(thetas[4]-driver_msg.destination[4]) < 0.0005 and \
-            abs(thetas[5]-driver_msg.destination[5]) < 0.0005 ):
-
+        if all(abs(thetas[i] - driver_msg.destination[i]) < 0.0005 for i in range(6)):
             at_goal = 1
-            #rospy.loginfo("Goal is reached!")
 
         loop_rate.sleep()
 
-        if(spin_count >  SPIN_RATE*5):
-
+        if(spin_count > SPIN_RATE*5):
             pub_cmd.publish(driver_msg)
             rospy.loginfo("Just published again driver_msg")
             spin_count = 0
 
-        spin_count = spin_count + 1
+        spin_count += 1
 
     return error
 
-################ Pre-defined parameters and functions no need to change above ################
-
-
-def move_block(pub_cmd, loop_rate, start_xw_yw_zw, target_xw_yw_zw, vel, accel):
-
-    """
-    start_xw_yw_zw: where to pick up a block in global coordinates
-    target_xw_yw_zw: where to place the block in global coordinates
-
-    hint: you will use lab_invk(), gripper(), move_arm() functions to
-    pick and place a block
-
-    """
-    # ========================= Student's code starts here =========================
-
-    # global variable1
-    # global variable2
-    Dest_f = lab_invk(Dest[0], Dest[1], 0.03, 0)
-    # print(f"Dest_f = {Dest_f}")
-
-    move_arm(pub_cmd, loop_rate, new_dest, vel, accel)
-
-    gripper(pub_cmd, loop_rate, suction_on)
-    time.sleep(1.0)
-    if 1.2 < analog_in_0 < 2:
-        gripper(pub_cmd, loop_rate, suction_off)
-        move_arm(pub_cmd,loop_rate,go_away,4.0, 4.0)
-        error = -2
-        rospy.loginfo("Block not found Exiting")
-        sys.exit()
-    # Delay to make sure suction cup has grasped the block
-    move_arm(pub_cmd, loop_rate, go_away,4.0, 4.0)
-    move_arm(pub_cmd, loop_rate, Dest_f,4.0, 4.0)
-    gripper(pub_cmd, loop_rate, suction_off)
-    move_arm(pub_cmd, loop_rate, go_away,4.0, 4.0)
-    
-    error = 0
+def move_block(pub_cmd, loop_rate, start_xw_yw_zw, Dest, vel, accel):
 
     # ========================= Student's code ends here ===========================
 
-    return error
 
+    print("Moving block from", start_xw_yw_zw, "to", Dest)
+    
+    start_angles_safe = lab_invk(start_xw_yw_zw[0] + 0.002, start_xw_yw_zw[1] + 0.006, start_xw_yw_zw[2] + 0.05, 0)
+    start_angles = lab_invk(start_xw_yw_zw[0] + 0.002, start_xw_yw_zw[1] + 0.006, start_xw_yw_zw[2], 0)
+    target_angles = lab_invk(Dest[0], Dest[1], 0.04, 0)
+    
+
+    gripper(pub_cmd, loop_rate, suction_on)
+    move_arm(pub_cmd, loop_rate, start_angles_safe, vel, accel)
+    time.sleep(1.0)
+    move_arm(pub_cmd, loop_rate, start_angles, vel, accel)
+    time.sleep(1.0)
+    move_arm(pub_cmd, loop_rate, start_angles_safe, vel, accel)
+
+    if 1.2 < analog_in_0 < 2:
+        gripper(pub_cmd, loop_rate, suction_off)
+        move_arm(pub_cmd, loop_rate, go_away, vel, accel)
+        rospy.loginfo("Block not found. Exiting.")
+        
+        return -2
+
+    move_arm(pub_cmd, loop_rate, go_away, vel, accel)
+    move_arm(pub_cmd, loop_rate, target_angles, vel, accel)
+    gripper(pub_cmd, loop_rate, suction_off)
+    move_arm(pub_cmd, loop_rate, go_away, vel, accel)
+
+    return 0
 
 class ImageConverter:
-
     def __init__(self, SPIN_RATE):
-
         self.bridge = CvBridge()
         self.image_pub = rospy.Publisher("/image_converter/output_video", Image, queue_size=10)
         self.image_sub = rospy.Subscriber("/cv_camera_node/image_raw", Image, self.image_callback)
-
         self.loop_rate = rospy.Rate(SPIN_RATE)
 
-        # Check if ROS is ready for operation
         while(rospy.is_shutdown()):
             print("ROS is shutdown!")
 
-
     def image_callback(self, data):
- 
-        print("image_callback triggered")
-        global xw_yw_G # store found green blocks in this list
-        global xw_yw_Y # store found yellow blocks in this list
+        global xw_yw_G
+        global xw_yw_Y
 
         try:
-          # Convert ROS image to OpenCV image
             raw_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
@@ -262,49 +178,25 @@ class ImageConverter:
         cv2.imshow("Raw Feed Debug", cv_image)
         cv2.waitKey(1)
 
-        # You will need to call blob_search() function to find centers of green blocks
-        # and yellow blocks, and store the centers in xw_yw_G & xw_yw_Y respectively.
-
-        # If no blocks are found for a particular color, you can return an empty list,
-        # to xw_yw_G or xw_yw_Y.
-
-        # Remember, xw_yw_G & xw_yw_Y are in global coordinates, which means you will
-        # do coordinate transformation in the blob_search() function, namely, from
-        # the image frame to the global world frame.
-
         xw_yw_G = blob_search(cv_image, "green")
+        print("Green block coordinates:", xw_yw_G)
         xw_yw_Y = blob_search(cv_image, "yellow")
-        # xw_yw_O = blob_search(cv_image, "orange")
+        print("Yellow block coordinates:", xw_yw_Y)
 
-
-"""
-Program run from here
-"""
 def main():
-
     global go_away
     global xw_yw_R
     global xw_yw_G
 
-    # global variable1
-    # global variable2
-
-    # Initialize ROS node
     rospy.init_node('lab5node')
 
-    # Initialize publisher for ur3/command with buffer size of 10
     pub_command = rospy.Publisher('ur3/command', command, queue_size=10)
-
-    # Initialize subscriber to ur3/position & ur3/gripper_input and callback fuction
-    # each time data is published
     sub_position = rospy.Subscriber('ur3/position', position, position_callback)
     sub_input = rospy.Subscriber('ur3/gripper_input', gripper_input, input_callback)
 
-    # Check if ROS is ready for operation
     while(rospy.is_shutdown()):
         print("ROS is shutdown!")
 
-    # Initialize the rate to publish to ur3/command
     loop_rate = rospy.Rate(SPIN_RATE)
 
     vel = 4.0
@@ -313,53 +205,33 @@ def main():
 
     ic = ImageConverter(SPIN_RATE)
     time.sleep(5)
-
-    detected_green = []
-    detected_yellow = []
-    # ========================= Student's code starts here =========================
-
-    """
-    Hints: use the found xw_yw_G, xw_yw_Y to move the blocks correspondingly. You will
-    need to call move_block(pub_command, loop_rate, start_xw_yw_zw, target_xw_yw_zw, vel, accel)
-    """
-    while len(detected_green) == 0:  # Keep checking until blocks are found
-        print(f"xw_yw_G = {xw_yw_G}")
-        if len(xw_yw_G) == 2:
-            detected_green = xw_yw_G.copy()
-            print(f"detected_green = {detected_green}")
-            #print(f"detected_green = {detected_green}")
-            #print(f"detected_green[0][0] = {detected_green[0][0]}")
-            new_dest1 = lab_invk(detected_green[0][0], detected_green[0][1], 0.03, 0)
-            new_dest2 = lab_invk(detected_green[1][0], detected_green[1][1], 0.03, 0)
-            move_block(pub_command, loop_rate, new_dest1, Dest[0], vel, accel)
-            move_block(pub_command, loop_rate, new_dest2, Dest[1], vel, accel)
-        rospy.sleep(0.5)  # Add small delay between checks
-        print("Searching for green blocks...")
     
-    while len(detected_yellow) == 0:  # Keep checking until blocks are found
-        if len(xw_yw_Y) == 2:
-            detected_yellow = xw_yw_Y.copy()
-            print(f"detected_yellow = {detected_yellow}")
-            new_dest1 = lab_invk(detected_yellow[0][0], detected_yellow[0][1], 0.025, 0)
-            new_dest2 = lab_invk(detected_yellow[1][0], detected_yellow[1][1], 0.025, 0)
-            move_block(pub_command, loop_rate, new_dest1, Dest[2], vel, accel)
-            move_block(pub_command, loop_rate, new_dest2, Dest[3], vel, accel)
-        rospy.sleep(0.5)  # Add small delay between checks
-        print("Searching for yellow blocks...")
+    def is_close(p1, p2, threshold=0.1):
+        return np.linalg.norm(np.array(p1) - np.array(p2)) < threshold
+
+    placed_blocks = set()
     
+    while not rospy.is_shutdown():
+        for i, coords in enumerate(xw_yw_G):
+            if not any(is_close(coords, placed) for placed in placed_blocks):
+                move_block(pub_command, loop_rate, [coords[0], coords[1], 0.03], Dest[i % len(Dest)], vel, accel)
+                placed_blocks.add(tuple(Dest[i % len(Dest)]))
 
+        for i, coords in enumerate(xw_yw_Y):
+            if not any(is_close(coords, placed) for placed in placed_blocks):
+                move_block(pub_command, loop_rate, [coords[0], coords[1], 0.025], Dest[(i + 2) % len(Dest)], vel, accel)
+                placed_blocks.add(tuple(Dest[(i + 2) % len(Dest)]))
 
-    # ========================= Student's code ends here ===========================
+        rospy.sleep(1.0)
+
 
     move_arm(pub_command, loop_rate, go_away, vel, accel)
-    rospy.loginfo("Task Completed!")
+    rospy.loginfo("Task Completed.")
     print("Use Ctrl+C to exit program")
     rospy.spin()
 
 if __name__ == '__main__':
-
     try:
         main()
-    # When Ctrl+C is executed, it catches the exception
     except rospy.ROSInterruptException:
         pass
